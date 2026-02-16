0a46475028b5f0120ca12cdd94cfbe4621476ff1
Chizzydev <chizzy.dev250@gmail.com>
2026-02-16 23:17:15 +0100

    Disable Sentry in edge runtime

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,23 @@
-import { withSentryConfig } from "@sentry/nextjs";
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // optional, but recommended
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-       {
         protocol: 'https',
-        hostname: 'utfs.io', // ADD THIS FOR UPLOADTHING
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
       },
     ],
   },
-  // Add other Next.js config options here if needed
-};
-
-export default withSentryConfig(nextConfig, {
-  org: "chizzy-dev",
-  project: "javascript-nextjs",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  webpack: {
-    automaticVercelMonitors: true,
-    treeshake: {
-      removeDebugLogging: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
     },
   },
-});
+}
+
+export default nextConfig
